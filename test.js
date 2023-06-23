ac89b8baf399cf55aa9cd15f5ae6f093a6c63867
rafiqul-altersense <110759994+rafiqul-altersense@users.noreply.github.com>
2023-06-23 18:28:23 +0600

    init

---
+++ test.js
@@ -0,0 +1,20 @@
+
+let data = {
+    "key1": "value1",
+    "key2": 2,
+    "key3": {
+        "key1": "value1",
+        "key2": {
+            "key1": [
+                
+                {
+                "key1":"value1",
+            }, , {
+                    "key1": "value1",
+                },
+                {
+                    "key1": "value1",
+                }]
+        }
+    }
+}
